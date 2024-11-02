7338047173f4639b369aab0f0344b1a941226486
Matt Universe <ff8loser@gmail.com>
2024-11-02 14:24:35 -0400

    added support for inserting new blocks before existing ones

--- CodeRover/gameapp.js
+++ CodeRover/gameapp.js
@@ -1,14 +1,12 @@
 console.log("hello world test");
 
-function makeBlockElement(type, arg){
-    const element = document.createElement("div");
-}
-
 //prog block tools
 let ghost = null;
+let ghostMode = 0; //0:noghost, 1:follow cursor, 2:sticky
 let tool = null;
 let curBlockId = 0;
 const dragTools = interact(".tool");
+const progBlocks = interact(".prog_block");
 const progDropZone = interact(".program_area");
 const toolArea = document.getElementById("tool_area");
 const progArea = document.getElementById("program_area");
@@ -38,27 +36,47 @@ for(let t of tools){
 //spawn new prog blocks from available tools
 dragTools.draggable({
     listeners: {
-    start (event) {
-      //console.log(event.type, event.target);
-        //clone target and begin dragging clone
-        tool = event.target;
-        ghost = event.target.cloneNode(true);
-        ghost.classList.add("prog_ghost");
-        gameContainer.appendChild(ghost);
-        const rect = event.target.getBoundingClientRect();
-        position.x = event.clientX0;
-        position.y = event.clientY0;
-    },
-    move (event) {
-      position.x += event.dx;
-      position.y += event.dy;
-
-      ghost.style.transform =
-        `translate(${position.x}px, ${position.y}px)`;
-    },end(event){
-        ghost.remove();
+        start (event) {
+            //console.log(event.type, event.target);
+            //clone target and begin dragging clone
+            tool = event.target;
+            ghost = event.target.cloneNode(true);
+            ghostMode = 1;
+            ghost.classList.add("prog_ghost", "absolute");
+            gameContainer.appendChild(ghost);
+            const rect = event.target.getBoundingClientRect();
+            position.x = event.clientX0;
+            position.y = event.clientY0;
+        },
+        move (event) {
+            if(event.dragEnter){
+                //console.log("entered drop zone");
+                ghost.classList.remove("absolute");
+                ghost.style.transform = "inherit";
+                //event.dragEnter.appendChild(ghost);
+                ghostMode = 2;
+            } else if(event.dragLeave){
+                //console.log("left drop zone");
+                gameContainer.appendChild(ghost);
+                ghost.classList.add("absolute");
+                ghostMode = 1;
+            }
+            if(ghostMode == 1){
+                position.x += event.dx;
+                position.y += event.dy;
+                ghost.style.transform =
+                `translate(${position.x}px, ${position.y}px)`;
+            } else if(ghostMode == 2){
+                
+            }
+        },
+        end (event){
+            if(ghostMode == 1){
+                ghost.remove();
+                ghostMode = 0;
+            }
+        }
     }
-  }
 });
 
 function createBlockFromTool(){
@@ -69,18 +87,38 @@ function createBlockFromTool(){
     return newProgBlock;
 }
 
+function dropProgBlock(event){
+    if(event.relatedTarget.classList.contains("tool")){
+        console.log("a tool is being dropped");
+        ghost.after(createBlockFromTool());
+        tool = null;
+    } else{
+        
+    }
+    ghost.remove();
+    ghostMode = 0;
+}
+
 progDropZone.dropzone({
     accept: '.tool',
     ondrop: function(event){
-        console.log("drop recieved :"+event);
-        if(event.relatedTarget.classList.contains("tool")){
-            console.log("a tool is being dropped");
-            const newProgBlock = createBlockFromTool();
-            event.target.appendChild(newProgBlock);
-            tool = null;
-        } else{
-            
-        }
+        dropProgBlock(event);
+    },
+    ondragenter: function(event){
+        event.target.append(ghost);
+    },
+    ondragleave: function(event){
+        //console.log("drop zone left by drag")
+    },
+});
+
+progBlocks.dropzone({
+    accept: '.tool',
+    ondragenter: function(event){
+        event.target.before(ghost);
+    },
+    ondrop: function(event){
+        dropProgBlock(event);
     },
 });
 
